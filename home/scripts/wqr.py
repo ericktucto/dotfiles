2ffb6fcd2217b08326de3cbfe85c31b3a02f31b0
Erick Tucto <erick.tucto@outlook.com>
2021-09-30 18:30:21 -0500

    Updating wqr.py

--- home/scripts/wqr.py
+++ home/scripts/wqr.py
@@ -40,17 +40,17 @@ def obtener_password(ssid):
     lineas = obtener_informacion(ruta).split()
     r = re.compile("^psk=.*")
     resultado = [linea for linea in lineas if r.match(linea)]
-    return resultado[0][4:-1] if resultado else ""
+    return resultado[0][4:] if resultado else ""
 
 if __name__ == "__main__":
-    (temp_nombre, byWindow) = datos(sys.argv[1:])
-    ssid = temp_nombre + ".nmconnection"
-    if ssid in conexiones_guardadas():
-        password = obtener_password(ssid)
-        nombre = "WIFI:T:WPA;S:%s;P:%s;;" % (ssid, password)
+    (ssid, byWindow) = datos(sys.argv[1:])
+    nombre_conexion = ssid + ".nmconnection"
+    if nombre_conexion in conexiones_guardadas():
+        password = obtener_password(nombre_conexion)
+        nombre = f"WIFI:T:WPA;S:{ssid};P:{password};;"
         if byWindow:
             subprocess.run(["qrencode", "-s", "10", "-m", "8", "-o", "/tmp/wifi.png", nombre])
-            subprocess.run(["feh", "/tmp/wifi.png", "-^", temp_nombre])
+            subprocess.run(["feh", "/tmp/wifi.png", "-^", ssid])
         else:
             subprocess.run(["qrencode", "-t", "UTF8", nombre])
     else:
