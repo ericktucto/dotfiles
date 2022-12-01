0c7025cd4b5a5a6030b322be5c6d2ab71ac616cd
Erick Tucto <erick@ericktucto.com>
2022-12-01 16:10:43 -0500

    Clean gitstatus.py script

--- nvim/scripts/gitstatus.py
+++ nvim/scripts/gitstatus.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
-import sys
 import os
-import re
 import subprocess
 
-directorio_actual = os.getcwd()
-
-def comando(cmd, precmd = None) -> tuple:
+def comando(cmd) -> str:
     cmd = cmd.split() if type(cmd) == str else cmd
-    comando = subprocess.Popen(
-        cmd,
-        stdin=precmd if precmd is None else precmd.stdout,
-        stdout=subprocess.PIPE
-    )
+    comando = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     (result, _) = comando.communicate()
-    return (result.decode("UTF-8"), comando)
-
-
-
-(res, _) = comando('git status -s')
-a = [x for index, x in enumerate(res.split()) if index % 2]
+    return result.decode("UTF-8")
 
 def obtener_archivos(paths: list, reduce = [], prefijo = ""):
     for p in paths:
@@ -28,17 +15,20 @@ def obtener_archivos(paths: list, reduce = [], prefijo = ""):
             reduce.append(x)
         elif os.path.isdir(x):
             obtener_archivos(
-                comando(["ls", x])[0].split(),
+                comando(["ls", x]).split(),
                 reduce,
                 x if x[-1:] == "/" else x + "/"
             )
+        else:
+            continue
     return reduce
 
-print("\n".join(obtener_archivos(a)))
+res = comando('git status -s')
+print(
+    "\n".join(
+        obtener_archivos(
+            [x for index, x in enumerate(res.split()) if index % 2] # index % 2 get name file o name dir
+        )
+    )
+)
 
-"""
-archivo = sys.argv[1]
-resultado = "%s/%s" % (directorio_actual, archivo[2:])
-(ruta, linea, busqueda) = obtener(resultado)
-subprocess.run(["/home/erick/.dotfiles/nvim/pack/widget/start/fzf.vim/bin/preview.sh", f"{ruta}:{linea}"])
-"""
